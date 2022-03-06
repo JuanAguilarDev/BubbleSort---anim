31b0e44c08634af6a0d11490cb8e8953da3a381e
JuanAguilarDev <juan_aguilar1290@outlook.es>
2022-03-06 16:25:23 -0600

    Finish

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 var sendBtn = document.getElementById('send'); 
 var select = document.getElementById('select')
-var list = document.getElementById('array_elements');
+var list = document.querySelector('#array_elements');
+var sortBtn = document.getElementById('sort');
 var size = 0;
 var array = [];
 
@@ -14,27 +15,59 @@ sendBtn.addEventListener('click', ()=>{
     }else{
         fillArray(array);
         createLi(array);
-        
     }
 }); 
+
+sortBtn.addEventListener('click', ()=>{
+    if(array.length === 0){
+        alert('Crea un array primero. ')
+    }else{
+        sortLi(array);
+    }
+});
 
 function random(min, max){
     return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
 function createLi(arr){
-    for(var i = 0; i<arr.length; i++){
-        list.innerHTML += `<li>${arr[i]}</li>`;
+    list.innerHTML = "";
+    createContainer();
+    for(let i = 0; i<arr.length; i++){
+        list.innerHTML += `<li id="element" class="animate__animated animate__wobble"><a>${arr[i]}</a></li>`;
     }
 }
 
+function createContainer(){
+    list.className += "animate__animated animate__backInLeft";
+}
+
 function fillArray(arr){
-    for(var i = 0; i<size; i++){
+    arr.splice(0, arr.length); // Limpiamos el arreglo
+    for(let i = 0; i<size; i++){
         arr.push(random(0, 500));
+    }
+}
+
+function sortLi(arr){
+    list.innerHTML = "";
+    let sortedArray = bubbleSort(arr); 
+    for(let i = 0; i<sortedArray.length; i++){
+        list.innerHTML += `<li id="element" class="animate__animated animate__tada"><a>${sortedArray[i]}</a></li>`;
     }
 }
 
 
 function bubbleSort(arr){
-
+    // -1 compare only the n size of elements of the array
+    for(let i = 0; i < arr.length-1; i++ ){
+        for(let j = 0; j < arr.length-1; j++){
+            if(arr[j] > arr[j+1]){
+                let aux = arr[j]; 
+                arr[j] = arr[j+1]; 
+                arr[j+1] = aux;
+            }
+        }
+    }
+    return arr;
 }
